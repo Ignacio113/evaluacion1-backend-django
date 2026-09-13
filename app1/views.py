41ef3e2a59b04972b89b7fc0c2576641714ea7cc
Ignacio2077 <ignaciocampos2468@gmail.com>
2026-09-13 15:38:09 -0300

    Defino dos vistas en app1 y configuro rutas en urls.py

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def vista1(request):
+    return HttpResponse("<h1>Bienvenido a la Vista 1</h1>")
+
+def vista2(request):
+    return HttpResponse("<p>Esta es la Vista 2</p>")
